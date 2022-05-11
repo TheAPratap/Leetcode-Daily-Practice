ed61016d51790ebb923afd9903140143a3c9ecd8
Abhinav Pratap <TheAPratap@gmail.com>
2022-05-11 09:32:26 +0530

    Time: 1 ms (90.60%), Space: 41.5 MB (68.30%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,12 +1,12 @@
 import java.util.*;
 class Solution
 {
-    // public boolean arrayStringsAreEqual(String[] word1, String[] word2)
-    // {
-    //         return String.join("",word1).equals(String.join("",word2));
-    // }
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2)
+    {
+            return String.join("",word1).equals(String.join("",word2));
+    }
     
-    
+/*
     public boolean arrayStringsAreEqual(String[] word1, String[] word2)
     {
             return helper(word1).equals(helper(word2));
@@ -21,4 +21,5 @@ class Solution
         }
         return sb.toString();
     }
+*/
 }
