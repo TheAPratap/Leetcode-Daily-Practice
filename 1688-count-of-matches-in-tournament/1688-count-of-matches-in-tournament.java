4d9cd12c412b32c2d7e5069aa500155ee5a3bf55
Abhinav Pratap <TheAPratap@gmail.com>
2022-06-05 11:55:09 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (38.78%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -5,15 +5,15 @@ class Solution
         int match=0;
         while(n != 1)
         {
-            if(n%2 == 0)
-            {
-                match += n/2;
-                n = n/2;
-            }
-            else
+            if(n%2 != 0)
             {
                 match += (n-1)/2;
                 n = (n-1)/2 + 1;
+            }
+            else
+            {
+                match += n/2;
+                n = n/2;
             }
         }
         return match;
