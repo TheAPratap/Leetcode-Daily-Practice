456b6758289da8c65dd4099a0ebc9d6e461fa636
Abhinav Pratap <TheAPratap@gmail.com>
2022-06-07 10:06:54 +0530

    Time: 2 ms (32.28%), Space: 55.6 MB (32.08%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,14 +1,32 @@
+// class Solution
+// {
+//     public void reverseString(char[] s)
+//     {
+//         int left = 0;
+//         int right = s.length-1;
+//         while(left < right)
+//         {
+//             char temp = s[left];
+//             s[left++] = s[right];
+//             s[right--] = temp;
+//         }
+//     }
+// }
+
 class Solution
 {
+    private void recursive(char[] s, int left, int right)
+    {
+        if(left > right)
+            return;
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        recursive(s, left+1, right-1);
+    }
+    
     public void reverseString(char[] s)
     {
-        int left = 0;
-        int right = s.length-1;
-        while(left < right)
-        {
-            char temp = s[left];
-            s[left++] = s[right];
-            s[right--] = temp;
-        }
+        recursive(s, 0, s.length-1);
     }
 }
