919329769e5dc9356e422580754a503316a68167
Abhinav Pratap <TheAPratap@gmail.com>
2022-07-04 22:46:24 +0530

    Time: 1 ms (51.57%), Space: 43.3 MB (39.37%) - LeetHub

---
+++ 717-1-bit-and-2-bit-characters/717-1-bit-and-2-bit-characters.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public boolean isOneBitCharacter(int[] bits)
+    {
+        boolean flag=false;
+        for(int i=0;i<bits.length;i++)
+        {
+            if(i==bits.length-1)
+            {
+                flag=true;
+            }
+            if(bits[i]==1)
+            {
+                i++;
+            }
+        }
+        return flag;
+    }
+}
